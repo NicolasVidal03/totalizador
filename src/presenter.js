2662d01a72cfecf0204a06c069c4fe1cb9f99221
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-09 23:10:16 -0400

    Añadir el multiplicador

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,12 @@
+import multiplicar from "./multiplicador";
 import sumar from "./sumador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
+const form_mult = document.querySelector("#multiplicar-form");
 const div = document.querySelector("#resultado-div");
+const div_mult = document.querySelector("#resultado-mult-div"); 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -11,5 +14,14 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>Resultado suma: " + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form_mult.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div_mult.innerHTML = "<p>Resultado multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
 });
