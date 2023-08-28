39f011aa9ec3e3829cc910570c1d3a8a08a222eb
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-28 12:28:34 -0400

    Muestra precio del item

--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,12 @@
 const cantidad_items = document.querySelector("#cant-items");
+const precio_item = document.querySelector("#precio-item");
 const form = document.querySelector("#tot-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + "Cantidad: " + cantidad_items.value + "</p>"; 
+  div.innerHTML = "<p>" + "Cantidad: " + cantidad_items.value + 
+  "<br>Precio: " + precio_item.value + "</p>"; 
   
 });
