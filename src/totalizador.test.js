231ca111ab9de76e486dadb571dd797dc64e0d06
Nicolas <nicolas.vidal@ucb.edu.bo>
2023-08-28 12:25:32 -0400

    Muestra cantidad en pantalla

--- src/totalizador.test.js
+++ src/totalizador.test.js
@@ -5,6 +5,7 @@ const div = document.querySelector("#resultado-div");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  console.log(cantidad_items.value);
   div.innerHTML = "<p>" + "Cantidad: " + cantidad_items.value + "</p>"; 
   
 });
